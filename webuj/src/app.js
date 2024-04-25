75fcac5e686363ca29bdcdd84f4a892fb50b13b6
Ritzl Bence <ritzlbencelevente@ktch.hu>
2024-04-25 10:47:01 +0200

    2024-04-25

--- webuj/src/app.js
+++ webuj/src/app.js
@@ -1,6 +1,7 @@
 const doc = {
     empBody: document.querySelector("#empBody"),
     addButton: document.querySelector("#addButton"),
+    idDoc: document.querySelector("#id"),
     nameDoc: document.querySelector("#name"),
     cityDoc: document.querySelector("#city"),
     salaryDoc: document.querySelector("#salary")
@@ -10,7 +11,9 @@ const state = {
      url: "http://localhost:8000/employees",
      name: "",
      city: "",
-     salary: 0
+     salary: 0,
+     add: true
+     
     
 }
 
@@ -18,7 +21,13 @@ const state = {
 
 doc.addButton.addEventListener("click", () => {
     console.log("Működik")
+    
+    getDataFromForm()
     createEmployee()
+    deleteModalContent()
+    
+    getEmployees()
+    
 })
 
 function getDataFromForm() {
@@ -54,7 +63,11 @@ function getEmployees() {
     
     fetch(state.url)
     .then( response => response.json())
-    .then(result => {renderEmployees(result)})  //lekérés, aszinkron utasítás, névtelen függvény. Ha van neve, akkor nem kell a (zárójel), ha csak egy utasítás van, akkor nem kell a {kapcsos zárójel} sem.
+    .then(result => { 
+        clearTableContent() 
+        renderEmployees(result)
+    
+    })  //lekérés, aszinkron utasítás, névtelen függvény. Ha van neve, akkor nem kell a (zárójel), ha csak egy utasítás van, akkor nem kell a {kapcsos zárójel} sem.
     // Ha nem hagyom el a {kapcsos zárójelet}, akkor kell a return
     // fetch --> promise-t ad vissza --> használható a then utasítás
 }
@@ -70,14 +83,65 @@ function renderEmployees(employeeList){
             <td>${emp.city}</td>
             <td>${emp.salary}</td>
 
-            <td>
-                <button class="btn btn-outline-success">Szerkesztés</button>
-                <button class="btn btn-outline-danger">Törlés</button>
+            <td class="width">
+                <button class="btn btn-outline-success" 
+                data-id="${emp.id}"
+                data-name="${emp.name}"
+                data-city="${emp.city}"
+                data-salary="${emp.salary}"
+                data-bs-toggle="modal" data-bs-target="#staticBackdrop"
+                 
+                onclick="startEdit(this)">Szerkesztés <i class="bi bi-pencil-fill"></i></button>
+                <button class="btn btn-outline-danger" onclick="startDelete(${emp.id})">Törlés <i class="bi bi-trash3"></i></button>
             </td>
         `
         doc.empBody.appendChild(row)
     });
+}
+
+
+function deleteModalContent() {
+    doc.idDoc.value = "";
+    doc.nameDoc.value = "";
+    doc.cityDoc.value = "";
+    doc.salaryDoc.value = "";
 
 }
+
+function clearTableContent() {
+    doc.empBody.textContent = "";
+}
+
+function startDelete(id) {
+    console.log("törlendő:")
+    deleteEmployee(id)
+    getEmployees()
+}
+
+function deleteEmployee(id) {
+    let newUrl = state.url + "/"+id
+    fetch(newUrl, {
+        method: "delete"
+    })
+}
+
+function startEdit(source) {
+    console.log("Szerkesztés...")
+    doc.idDoc.value = source.dataset.id
+    doc.nameDoc.value = source.dataset.name
+    doc.cityDoc.value = source.dataset.city
+    doc.salaryDoc.value = source.dataset.salary
+    
+    
+}
+
+function editEmployee(id) {
+    let newUrl = state.url + "/"
+}
+
+function startAdding() {
+    deleteModalContent();
+}
+
 
 getEmployees()
